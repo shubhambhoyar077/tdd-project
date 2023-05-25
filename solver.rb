d37df26a28236df1471863d31924ad8e6e5c9464
Dheeraj Sachdeva <dheerajarya@gmail.com>
2023-05-25 15:04:55 +0530

    added fizzbuzz logic

--- solver.rb
+++ solver.rb
@@ -10,8 +10,10 @@ class Solver
   end
 
   def fizzbuzz(number)
+  return "fizzbuzz" if (number%3 == 0 && number%5 == 0)
 return "fizz" if number%3 == 0 
 return "buzz" if number%5 == 0 
+
   end
 
 
