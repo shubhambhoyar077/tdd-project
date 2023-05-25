b290d6412ff5c9814f574020d241b75230214a6d
Dheeraj Sachdeva <dheerajarya@gmail.com>
2023-05-25 15:07:42 +0530

    Added logic if number not divided by 3 and 5

--- solver.rb
+++ solver.rb
@@ -10,10 +10,10 @@ class Solver
   end
 
   def fizzbuzz(number)
-  return "fizzbuzz" if (number%3 == 0 && number%5 == 0)
-return "fizz" if number%3 == 0 
-return "buzz" if number%5 == 0 
-
+    return "fizzbuzz" if (number%3 == 0 && number%5 == 0)
+    return "fizz" if number%3 == 0 
+     return "buzz" if number%5 == 0 
+  number.to_s
   end
 
 
