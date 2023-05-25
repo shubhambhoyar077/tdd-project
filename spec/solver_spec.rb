c474c0d60ee0e451134aa023bd3e9edab7860308
shubham <shubham.bhoyar077@gmail.com>
2023-05-25 14:21:21 +0530

    Add test for 0 factorial

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,4 +7,8 @@ describe Solver do
   it "test for 4 factorial should return 24" do
     expect(@solver.factorial(4)).to eq(24)
   end
+
+  it "test for 0 factorial should return 1" do
+    expect(@solver.factorial(0)).to eq(1)
+  end
 end
